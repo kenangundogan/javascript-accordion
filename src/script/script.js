b26f5e5c27ed83cd39b6376abff6a58e2ea4484e
Kenan Gundogan <kenangundogann@gmail.com>
2021-04-04 13:31:43 +0300

    Update script

--- src/script/script.js
+++ src/script/script.js
@@ -1,42 +1,46 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-    function accordionWrapperFunc(itemWrapper) {
+    function accordionWrapperFunc(itemWrapper, itemWrapperTransition) {
         itemWrapper.querySelectorAll("li").forEach(i => {
             if (i.className == "show") {
                 i.querySelector(".body").style.height = "";
                 setTimeout(() => {
                     i.classList.remove("show");
-                }, 300);
+                    i.querySelector(".body").style = "";
+                }, itemWrapperTransition);
             }
         });
     }
 
-    function accordionItemFunc(itemWrapper, i) {
+    function accordionItemFunc(itemWrapper, itemWrapperTransition, i) {
         var item = i.closest("li");
         var activeItem = item.className == "show";
         var itemBody = item.querySelector(".body");
         var itemBodyWrapper = item.querySelector(".wrapper");
         if (!activeItem) {
-            accordionWrapperFunc(itemWrapper);
+            accordionWrapperFunc(itemWrapper, itemWrapperTransition);
             item.classList.add("show");
-            var itemHeight = itemBodyWrapper.offsetHeight + "px";
-            itemBody.style.height = itemHeight;
+            itemBody.style.transition = itemWrapperTransition + "ms";
+            itemBody.style.height = itemBodyWrapper.offsetHeight + "px";
         } else {
             itemBody.style.height = "";
             setTimeout(() => {
                 item.classList.remove("show");
-            }, 300);
+                itemBody.style = "";
+            }, itemWrapperTransition);
         }
     }
 
-    function accordionFunc(params) {
+    function accordionFunc() {
         let itemHead = document.querySelectorAll(".javascript-accordion li .head");
         if (itemHead) {
             itemHead.forEach(i => {
                 let itemWrapper = i.closest("ul");
                 let itemWrapperEvent = itemWrapper.getAttribute("data-accordion-event");
+                console.log(itemWrapper.getAttribute("data-accordion-transition"));
+                let itemWrapperTransition = itemWrapper.getAttribute("data-accordion-transition") == null ? "0" : itemWrapper.getAttribute("data-accordion-transition");
                 i.addEventListener(itemWrapperEvent == null ? "click" : itemWrapperEvent, function () {
-                    accordionItemFunc(itemWrapper, i);
+                    accordionItemFunc(itemWrapper, itemWrapperTransition, i);
                 })
             });
         }
